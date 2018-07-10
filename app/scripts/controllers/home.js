80ffc771197768f33c452f2f50249653111503de
Emerson Castilho <emerson.castilho@db1.com.br>
2018-07-10 13:47:00 -0300

    Create wizzard validation between steps

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -21,19 +21,15 @@ angular.module('app')
     });
 
 function homeCtrl($scope, $q, $timeout, $uibModal, rankingService, contaService, WizardHandler, toastr) {
+
+    $scope.showDeckDataValidation = false;
+    $scope.showDeckAuthentication = false;
+    $scope.showDeckSessionManagement = false;
+    $scope.showDeckAuthorization = false;
+    $scope.showDeckCryptography = false;
    
     var wizardInit = true;
     $scope.$on('wizard:stepChanged', function(event, data) {
-       
-        //step shoose deck cards
-        if(data.index === 3){           
-           var filtered = _.where($scope.deckCards, {ticked: true});
-           if(filtered.length === 0){
-                toastr.error('Please, choose one card of deck.');     
-                return false;
-           }
-        } 
-
         if (wizardInit) {
             wizardInit = false;
             setTimeout(function() {
@@ -41,137 +37,74 @@ function homeCtrl($scope, $q, $timeout, $uibModal, rankingService, contaService,
                 $scope.$apply();
             });
         }
-    });
+    });   
 
-    $scope.enterValidation = function(){
-        return true;
-    };
-    
-    $scope.exitValidation = function(){
-        return true;
-    };
-    //example using context object
-    $scope.exitValidation = function(context){
-        return context.firstName === "Jacob";
-    }
-    //example using promises
-    $scope.exitValidation = function(){
-        var d = $q.defer();
-        $timeout(function(){
-            return d.resolve(true);
-        }, 1000);
-        return d.promise;
-    }
-
-
-    $scope.deckCards = [
-        {	icon: "<i class='fa fa-lg fa-code' style='color: #7a747b;'></i>",  maker: "Data validation and encoding",	ticked: false	},
-        {	icon: "<i class='fa fa-lg fa-user-secret' style='color: #5f8ea0;'></i>", maker: "Authentication",	ticked: false	},
-        {	icon: "<i class='fa fa-lg fa-cogs' style='color: #5b906e;'></i>", maker: "Session management",	ticked: false	},
-        {	icon: "<i class='fa fa-lg fa-user-times' style='color: #b59b6c;'></i>",	maker: "Authorization",	ticked: false	},
-        {	icon: "<i class='fa fa-lg fa-unlock-alt' style='color: #895091;'></i>",	maker: "Cryptography",	ticked: false	}
+    $scope.players = [
+        {	icon: "<i class='fa fa-lg fa-user' style='color: #7a747b;'></i>", name: "Emershow",  maker: "Emerson", selected: false	},
+        {	icon: "<i class='fa fa-lg fa-user' style='color: #7a747b;'></i>", name: "Fusca", maker: "Wagner", selected: false	},
+        {	icon: "<i class='fa fa-lg fa-user' style='color: #7a747b;'></i>", name: "Maikon", maker: "Maikon", selected: false	},
+        {	icon: "<i class='fa fa-lg fa-user' style='color: #7a747b;'></i>", name: "Cachhoeira",	maker: "Cachoeira",	selected: false	},
+        {	icon: "<i class='fa fa-lg fa-user' style='color: #7a747b;'></i>", name: "Vini",	maker: "Vinicius",	selected: false	},
+        {	icon: "<i class='fa fa-lg fa-user' style='color: #7a747b;'></i>", name: "Tiuzão",	maker: "Edoil",	selected: false	}
    ];
 
+    $scope.changeStepPlayersToDeckSuitTs = function() {
+        var filtered = _.where($scope.players, {selected: true});
+        if(filtered.length <= 3){
+            toastr.error('Please, choose ate least 4 challengers.');     
+            return false;
+        }
+        WizardHandler.wizard().next();
+    };
+
+    $scope.deckCards = [
+        {	icon: "<i class='fa fa-lg fa-code' style='color: #7a747b;'></i>", idDeck: "1",  name: "Data validation and encoding",	selected: false	},
+        {	icon: "<i class='fa fa-lg fa-user-secret' style='color: #5f8ea0;'></i>", idDeck: "2", name: "Authentication",	selected: false	},
+        {	icon: "<i class='fa fa-lg fa-cogs' style='color: #5b906e;'></i>", idDeck: "3", name: "Session management",	selected: false	},
+        {	icon: "<i class='fa fa-lg fa-user-times' style='color: #b59b6c;'></i>", idDeck: "4",	name: "Authorization",	selected: false	},
+        {	icon: "<i class='fa fa-lg fa-unlock-alt' style='color: #895091;'></i>", idDeck: "5",	name: "Cryptography",	selected: false	}
+   ];
+
+    $scope.changeStepDeckSuitToStartTheGame = function() {
+        var filtered = _.where($scope.deckCards, {selected: true});
+        if(filtered.length === 0){
+            toastr.error('Please, choose one card of deck.');     
+            return false;
+        }
+
+        $scope.showDeckDataValidation = false;
+        $scope.showDeckAuthentication = false;
+        $scope.showDeckSessionManagement = false;
+        $scope.showDeckAuthorization = false;
+        $scope.showDeckCryptography = false;
+
+        _.each(filtered, function(deck) {
+            switch(deck.idDeck) {
+                case "1":
+                    $scope.showDeckDataValidation = true;
+                    break;
+                case "2":
+                    $scope.showDeckAuthentication = true;
+                    break;
+                case "3":
+                    $scope.showDeckSessionManagement = true;
+                    break;
+                case "4":
+                    $scope.showDeckAuthorization = true;
+                    break;
+                case "5":
+                    $scope.showDeckCryptography = true;
+                    break;                    
+                default:
+                    $scope.showDeckDataValidation = false;
+                    $scope.showDeckAuthentication = false;
+                    $scope.showDeckSessionManagement = false;
+                    $scope.showDeckAuthorization = false;
+                    $scope.showDeckCryptography = false;
+            }
+        });
 
 
-
-
-
-
-
-
-
-
-
-
-    // function nomeCompleto(primerioNome, ultimoNome) {
-    //     if (primerioNome === null || primerioNome === "") {
-    //         return "Sem nome";
-    //     }
-    //     if (ultimoNome === null || ultimoNome === "") {
-    //         return primerioNome;
-    //     }
-    //     return primerioNome + " " + ultimoNome;
-    // }
-
-    // contaService.obterDadosUsuarioLogado().then(function(usuario) {
-    //     var fname = usuario.firstName;
-    //     var firstName = fname == null || fname == '' ? '' : usuario.firstName.split(" ");
-    //     $scope.usuarioLogado = firstName[0];
-
-    //     rankingService.obterRankingGeral(usuario.uuidEmpresa).then(function(rankingGeral) {
-    //         $scope.rankingGeral = _.map(rankingGeral.rankings, function(ranking) {
-    //             return {
-    //                 "posicao": ranking.posicao + "°",
-    //                 "variacaoPosicao": ranking.variacaoPosicao || 0,
-    //                 "nome": nomeCompleto(ranking.userDTO.firstName, ranking.userDTO.lastName),
-    //                 "quantidadeAcertos": ranking.quantidadeAcertos,
-    //                 "quantidadeAcertosExatos": ranking.quantidadeAcertosExatos,
-    //                 "quantidadeErros": ranking.quantidadeErros,
-    //                 "pontos": ranking.pontos,
-    //                 "imageUrl": ranking.userDTO.imageUrl
-    //             };
-    //         });
-
-    //         if ($scope.rankingGeral === null) {
-    //             $scope.mostraMelhoresDoRank = false;
-    //         } else {
-    //             $scope.mostraMelhoresDoRank = true;
-    //             $scope.primeiroDoRankingGeral = $scope.rankingGeral[0].nome;
-    //         }
-    //         $scope.ultimoProcessamento = 
-    //         (rankingGeral.dataHoraProcessamento ? rankingGeral.dataHoraProcessamento : '-');
-
-    //     });
-
-    //     rankingService.obterRankingRodada(usuario.uuidEmpresa).then(function(rankingRodada) {
-    //         $scope.rankingRodada = _.map(rankingRodada.rankings, function(ranking) {
-    //             return {
-    //                 "posicao": ranking.posicao + "°",
-    //                 "variacaoPosicao": ranking.variacaoPosicao || 0,
-    //                 "nome": nomeCompleto(ranking.userDTO.firstName, ranking.userDTO.lastName),
-    //                 "quantidadeAcertos": ranking.quantidadeAcertos,
-    //                 "quantidadeAcertosExatos": ranking.quantidadeAcertosExatos,
-    //                 "quantidadeErros": ranking.quantidadeErros,
-    //                 "pontos": ranking.pontos,
-    //                 "imageUrl": ranking.userDTO.imageUrl
-    //             };
-    //         });
-
-    //         $scope.primeiroDoRankingDaRodada = $scope.rankingRodada[0].nome;
-    //     });
-
-    // });
-
-    // $scope.mostrarRegulamento = function() {
-    //     $uibModal.open({
-    //         animation: true,
-    //         templateUrl: "views/modalregulamento.html",
-    //         size: "lg",
-    //         controller: function($scope, $uibModalInstance) {
-    //             $scope.titulo = "Regulamento";
-    //             $scope.descricaoRegulamento = " 1 - Apostas e alterações de apostas são permitidas em até 2 horas antes do jogo. " +
-    //                 " 2 - Não é permitido a mudança ou envio de novas apostas para jogos finalizados. " +
-    //                 " 3 - São computados 3 pontos para resultados exatos e 1 ponto para acertos parciais." +
-    //                 " 4 - Após informado o resultado, o ranking será processado a cada cinco minutos." +
-    //                 " 5 - Não há no sistema, qualquer menu administrativo para mudança de apostas, portanto, após encerradas as apostas, " +
-    //                 "a mesma não pode ser alterada nem mesmo pelos administradores. ";
-
-    //             $scope.fecharRegulamento = function() {
-    //                 $uibModalInstance.close();
-    //             }
-    //         }
-    //     });
-    // }
-
-    
-    // $scope.obterRanking = function(tiporanking) {
-    //     if (tiporanking === "1") {
-    //         $scope.mostrarRankingRodada = false;
-    //         $scope.mostrarRankingGeral = true;
-    //     } else {
-    //         $scope.mostrarRankingRodada = true;
-    //         $scope.mostrarRankingGeral = false;
-    //     }
-    // };
+        WizardHandler.wizard().next();
+    };    
 }
